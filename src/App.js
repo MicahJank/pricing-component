01aeec67b201c03895d0e7ca64d973c6013f96a6
MicahJank <MicahJank@users.noreply.github.com>
2020-10-06 18:34:08 -0500

    added components, installed sass and removed the button component since i dont think i will need it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Toggle from './components/Toggle.js';
+import Card from './components/Card.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toggle />
+      <div className="card-section">
+        <Card name="Basic" />
+        <Card name="Professional" />
+        <Card name="Master" />
+      </div>
     </div>
   );
 }
